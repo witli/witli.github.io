07d7ebe41dad58b2156242ccbf87f89b4c3468b5
witli <35156657+witli@users.noreply.github.com>
2018-01-06 15:03:02 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,25 @@
+var myImage=document.querySelector('img');
+myImage.onclick=function(){
+    var mySrc=myImage.getAttribute('src');
+    if(mySrc=='images/huohu_logo.jpg'){
+        myImage.setAttribute('src','images/gou.jpg');
+    }else{
+        myImage.setAttribute('src','images/huohu_logo.jpg');
+    }
+}
+var myButton=document.querySelector('button');
+var myHeading=document.querySelector('h1');
+function setUserName(){
+    var myName=prompt('Please enter your name');
+    localStorage.setItem('name',myName);
+    myHeading.innerHTML='Mozilla is cool,'+myName;
+}
+if(!localStorage.getItem('name')){
+    setUserName();
+}else{
+    var storedName=localStorage.getItem('name');
+    myHeading.innerHTML='Mozilla is cool,'+storedName;
+}
+myButton.onclick=function(){
+    setUserName();
+}
